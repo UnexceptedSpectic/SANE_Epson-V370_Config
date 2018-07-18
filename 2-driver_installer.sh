4f5a9e2ee75995d4ee397e74715f1b926aaeca34
UnexceptedSpectic <pbadzuh@uncc.edu>
2018-07-18 17:19:52 -0400

    Update 2-driver_installer.sh

--- 2-driver_installer.sh
+++ 2-driver_installer.sh
@@ -2,10 +2,14 @@
 
 #installs legacy drivers
 
-unzip legacy_driver_bundle.zip
-cd legacy_driver_bundle
-sudo dpkg -i *.deb
+unzip new_drivers_07182018.zip
+sudo dpkg -i iscan_2.30.2-2_amd64.deb
 sudo apt-get install -f -y
+sudo dpkg -i iscan-data_1.36.0-1_all.deb
+sudo apt-get install -f -y
+sudo dpkg -i iscan-plugin-perfection-v370_1.0.0-2_amd64.deb
+sudo apt-get install -f -y
+
 sudo /etc/init.d/udev restart
 
 echo "Reboot to secure changes"
